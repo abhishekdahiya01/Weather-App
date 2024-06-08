ffb922354b3d04e7d27f962d3829010e07eb72a4
Abhishek Dahiya <153518031+abhishekdahiya01@users.noreply.github.com>
2024-06-08 22:53:42 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,15 @@
+
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const button = document.querySelector("#btn");
+
+async function getFacts() {
+  console.log("getting data.......");
+  let response = await fetch(URL);
+  console.log(response);
+  let data = await response.json();
+  console.log(data[1].text);
+  factPara.innerText = data[1].text;
+}
+
+button.addEventListener("click", getFacts);
